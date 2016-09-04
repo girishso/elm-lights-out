7d873a2eb4bac2e715c4d3e979621e009eaa9990
Girish S <girish.sonawane@gmail.com>
2016-09-04 19:57:22 +0530

    added toggle

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Html.App exposing (beginnerProgram)
 
@@ -12,7 +12,7 @@ import Html.App exposing (beginnerProgram)
 main : Program Never
 main =
     beginnerProgram
-        { model = model
+        { model = init
         , view = view
         , update = update
         }
@@ -23,21 +23,19 @@ main =
 
 
 type alias Model =
-    Int
+    {isOn: List Bool}
 
 
-model : Model
-model =
-    0
-
+--init : Model
+init =
+    {isOn = [True, True, True]}
 
 
 -- # Messages
 
 
 type Msg
-    = Inc
-    | Dec
+    = Toggle Int
 
 
 
@@ -47,22 +45,50 @@ type Msg
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Inc ->
-            model + 1
-
-        Dec ->
-            model - 1
+        Toggle ix ->
+            {model | isOn = toggleLights ix model.isOn}
 
 
-
+toggleLights : Int -> List Bool -> List Bool
+toggleLights ix list =
+    List.indexedMap
+        (\i isOn ->
+            if ix == i then
+                not isOn
+            else if ix == i + 1 then
+                not isOn
+            else if ix == i - 1 then
+                not isOn
+            else
+                isOn
+        ) list
 -- # View
 
 
 view : Model -> Html Msg
 view model =
     div [ class "container" ]
-        [ h1 [] [ text "Counterlicious" ]
-        , div [] [ text "Count: ", text (toString model) ]
-        , button [ onClick Dec ] [ text "Decrement" ]
-        , button [ onClick Inc ] [ text "Increment" ]
+    [
+        model.isOn
+            |> List.indexedMap btn
+            |> div []
+
+    ]
+
+btn : Int -> Bool -> Html Msg
+btn ix isOn =
+    div [
+            style [
+            ("background-color",
+                if isOn then
+                    "orange"
+                else
+                    "grey")
+            , ( "width", "40px" )
+            , ( "height", "40px" )
+            , ( "margin", "2px" )
+            , ( "display", "inline-block" )
+            ]
+            , onClick (Toggle ix)
         ]
+        []
